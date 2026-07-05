11928a150d6a67650caa84f5e3cb7054ed9824de
RayanIslam30 <rayislam2007@gmail.com>
2026-07-05 15:30:20 -0400

    added tip.py file

---
+++ tip.py
@@ -0,0 +1,18 @@
+#Code to calculate the tip based on the meal cost and tip percentage
+
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # incomplete
+
+
+def percent_to_float(p):
+    # incomplete
+
+
+main()
