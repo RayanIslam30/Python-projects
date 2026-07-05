ccdb65faf9af723241d1d0a8c678f61baf9aa3f2
RayanIslam30 <rayislam2007@gmail.com>
2026-07-05 10:25:24 -0400

    updated descriptions

--- indoor.py
+++ indoor.py
@@ -1,4 +1,5 @@
 #Code to prompt user for input and convert it to lowercase
+
 indoor = input("Please input a string: ") #prompt user for input
 indoor = indoor.lower() #put the string in lowercase
 print(indoor)  #print lowercase string
--- playback.py
+++ playback.py
@@ -1,2 +1,4 @@
+#Code to prompt user for input and replace spaces with three ellipses
+
 playback = input("Please input a string: ") #prompt user for input
 playback = playback.replace(" ","...") #replace spaces with three ellipses
