7aa22ca6235e76102557d3237ffef424a8acaf4d
RayanIslam30 <rayislam2007@gmail.com>
2026-07-07 11:42:14 -0400

    fixed bank.py using startswith string method

--- bank.py
+++ bank.py
@@ -1,10 +1,10 @@
-#Code to output $0 when user inputs "hello", $20 when user inputs anything else starting with "h", and $100 otherwise
+#Code to output $0 when user inputs a string starting with "hello", $20 when user inputs anything else starting with "h", and $100 otherwise
 
 bank = input("Enter greeting: ") #prompt user for input
 
 clean_bank = bank.lower().strip() #put the string in lowercase and remove any whitespace
 #ifelse statement to check if the input is "hello", anything else starting with "h", and print "$0", "$20", or "$100" accordingly
-if clean_bank == "hello":
+if clean_bank.startswith("hello"):
     print("$0")
 elif clean_bank.startswith("h"):
     print("$20")
