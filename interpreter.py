7cdb7a624bc68c4827cb74db5dc71c45331b58d8
RayanIslam30 <rayislam2007@gmail.com>
2026-07-09 17:01:09 -0400

    created a basic calculator using elif

---
+++ interpreter.py
@@ -0,0 +1,18 @@
+#Code to take two integers and either add, subtract, multiply or divide them based on user input
+
+user_input = input("Enter your equation, with a space between each number and operator: ") #Take the input from the user
+x, y, z = user_input.split()
+x = int(x)
+z = int(z)
+
+#ifelse statement to check the operator and perform the corresponding operation
+if y == "+":
+    print(x + z)
+elif y == "-":
+    print(x - z)
+elif y == "*":
+    print(x * z)
+elif y == "/":
+    print(x / z)
+else:
+    print("Invalid operator. Please use +, -, *, or /")
