e4bc36c91c3712d53580a88a2437874d992d5182
RayanIslam30 <rayislam2007@gmail.com>
2026-07-13 13:59:37 -0400

    added secondary solution for fuel.py

--- fuel.py
+++ fuel.py
@@ -29,3 +29,30 @@ def gauge(percentage):
         print(f"{percentage:.0f}%")
 
 main()
+
+#Alternate solution having main do the conversion
+
+#def main():
+  #  while True:
+#        try:
+#           fraction = input("Fraction: ")
+#           percentage = convert(fraction)
+#           break
+#        except (ValueError, ZeroDivisionError):
+#           pass
+
+#    gauge(percentage)
+
+#def convert(fraction):
+#    x, y = fraction.split("/")
+ #   x = int(x)
+  #  y = int(y)
+#
+ #   if y == 0:
+  #      raise ZeroDivisionError
+   # if x < 0 or x > y:
+    #    raise ValueError
+
+    #return x / y * 100
+
+#main()
