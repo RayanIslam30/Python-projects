02bd56a9f8afdd2deef8a84d0560e7a37c0f9234
RayanIslam30 <rayislam2007@gmail.com>
2026-07-14 20:53:21 -0400

    added dictionary for menu

---
+++ taqueria.py
@@ -0,0 +1,14 @@
+#Code that allows the user to order from a taqueria menu and calculates the total cost of the order, incrementing the total cost as items are added to the order.
+
+#This is a dictionary that contains the menu items and their corresponding prices. The keys are the names of the menu items, and the values are the prices of those items.
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
