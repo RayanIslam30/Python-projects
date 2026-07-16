b5600e33e5d45d2961cde1677086f2c47ca33b5d
RayanIslam30 <rayislam2007@gmail.com>
2026-07-16 13:29:29 -0400

    finished program to take names user inputs and print them out with proper formatting

--- adieu.py
+++ adieu.py
@@ -1,13 +1,17 @@
 #Code to ask user for names until they quit, and then say 'adieu' to all the names after
 
 import inflect #Import module to properly format names
+p=inflect.engine()
 
 names = [] #Empty list to store names inputted by user
-
-while True:
+#Loop to get names, quit loop if ctrl-d inputted
+while True: 
     try:
         name = input("Name: ") #Get name from user
         names.append(name) #Add name to list
     except EOFError: #If user inputs ctrl-d, end
+        print() #Put out a new line for formatting reasons
         break
 #Print our string with proper formatting and 'adieu'
+print("Adieu, adieu, to ", end="")
+print(p.join(names))
