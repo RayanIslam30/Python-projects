41e5b5164c3821f9ecdd2e002ba7179905c46f8d
RayanIslam30 <rayislam2007@gmail.com>
2026-07-16 16:04:50 -0400

    working on code to randomly generate math problems for user to solve

--- game.py
+++ game.py
@@ -1,27 +1,29 @@
-#A guessing game that uses the random module to choose a number between 1 and the number the user inputs
-import random #Import random module
+# A guessing game that uses the random module to choose a number between 1 and the number the user inputs
+import random  # Import random module
 
-#Keep asking for user to input level until they input a valid number
-while True: 
+# Keep asking for user to input level until they input a valid number
+while True:
     try:
-        level = int(input("Level: ")) #User chooses 'level', the number inputted here is the upper range of guessable numbers
-        if level >=1:
+        level = int(
+            input("Level: ")
+        )  # User chooses 'level', the number inputted here is the upper range of guessable numbers
+        if level >= 1:
             break
     except ValueError:
         pass
 
 
-number = random.randint(1,level)
-#Loop to allow user to keep guessing until they get it right
+number = random.randint(1, level)
+# Loop to allow user to keep guessing until they get it right
 while True:
     try:
-        guess = int(input("Guess: ")) #User guesses a number
-        if guess == number: #If guess is right, tell user it's right and then break
+        guess = int(input("Guess: "))  # User guesses a number
+        if guess == number:  # If guess is right, tell user it's right and then break
             print("Just Right!")
             break
-        elif guess < number: #If guess is too small, tell user
+        elif guess < number:  # If guess is too small, tell user
             print("Too small!")
-        else: #If guess is too large, tell user
-            print("Too large!") 
-    except ValueError: # User didn't input number, ask again
+        else:  # If guess is too large, tell user
+            print("Too large!")
+    except ValueError:  # User didn't input number, ask again
         pass
---
+++ professor.py
@@ -0,0 +1,14 @@
+#Code that allows the user to choose between 3 levels, and solve 10 random math problems for that level
+import random 
+
+def main():
+    print("placeholder")
+
+def get_level():
+    print("placeholder")
+
+def generate_integer(level):
+    print("placeholder")
+
+if "__name__" == "__main__"
+    main()
