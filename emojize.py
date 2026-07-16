46f9f8adadd23de1918935b17b7e65138a064f6a
RayanIslam30 <rayislam2007@gmail.com>
2026-07-15 21:17:25 -0400

    used a library to convert emojis from name to the image

---
+++ emojize.py
@@ -0,0 +1,8 @@
+#Code that takes an emoji name and converts it to the actual emoji
+
+import emoji #Import emoji module which we use to convert
+
+emoji_name = input("Enter emoji text: ") #Prompt user for input
+
+print(emoji.emojize(emoji_name, language='alias')) #Convert either ':emoji_name:' or ':emojiname:' into the actual emoji
+
