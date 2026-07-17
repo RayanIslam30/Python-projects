4a39cf47f524aee8d9e4fa36f6aec4d48a6fcc7c
RayanIslam30 <rayislam2007@gmail.com>
2026-07-17 17:24:40 -0400

    added some error checking to the program

--- lines.py
+++ lines.py
@@ -3,10 +3,18 @@
 import sys #Import sys to use sys.argv
 lines = int(0) #Initialize our count of lines in the program
 
-name = sys.argv[1] #set our file name to file user inputted
+#Check that sys.argv[1] exists
+if len(sys.argv) == 2: #If file is valid, set name variable to file name
+    name = sys.argv[1] #set our file name to file user inputted
+elif len(sys.argv) == 1: #If sys.argv[1] doesn't exist, quit with too few arguments
+    sys.exit("Too few command-line arguments")
+else: #If there are more than 1 argument, quit with too many arguments
+    sys.exit("Too many command-line arguments")
 
+#Open the file and read it
 with open(name) as file:
-    for line in file:
-        lines+=1
+#Imp
+    for line in file: 
+        lines+=1 
 
 print(lines)
