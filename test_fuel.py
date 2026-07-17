8a6963d95aa192e92f82b687878324679128e2c7
RayanIslam30 <rayislam2007@gmail.com>
2026-07-17 09:44:06 -0400

    seperated tests logically

--- test_fuel.py
+++ test_fuel.py
@@ -10,15 +10,17 @@ def test_convert():
     assert convert("1/2") == 50 #Check that a normal fraction converts to percentage without %
     assert convert("1/1") == 100 #Check that it allows for 100
     assert convert("0/1") == 0 #Check that it allows for 0
+    
+#Test for ValueError in convert function
+def test_value_error():
     with pytest.raises(ValueError): #Check that it doesn't take non-integers
         convert("cat/dog")
-
-    with pytest.raises(ValueError): #Check that x cannot be negative, or exception is raised
         convert("-1/2")
-
-    with pytest.raises(ValueError): #Check that x/y cannot be greater than 1, or exception is raised
+        convert("1/-2")
         convert("2/1")
 
+#Test for ZeroDivisionError in convert function
+def test_zero_division_error():
     with pytest.raises(ZeroDivisionError): #Check that y cannot be 0, or exception is raised
         convert("1/0")
 
@@ -28,6 +30,3 @@ def test_gauge():
     assert gauge(66.67) == "67%" #Check rounding
     assert gauge(1) == "E" #Check that <=1 == E
     assert gauge(99) == "F" #Check that >=99 == F
-
-
-
