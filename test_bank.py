0980dad0725cb784c12648fe29b8bacc09a1977b
RayanIslam30 <rayislam2007@gmail.com>
2026-07-16 23:30:02 -0400

    added tests for finding value in bank.py

--- test_bank.py
+++ test_bank.py
@@ -4,4 +4,7 @@ from bank import value #Import the value function from bank
 #Our tests for the value function
 
 def test_value():
-    assert 
+    assert value("hello") == 0 #hello gives 0
+    assert value("hey") == 20 #greeting starting with 'h' gives 20
+    assert value("yo") == 100 #greeting not starting with 'h' gives 100
+    assert value("hello, User") == 0 #more text after hellow doesn't matter
