66fd561a722b609c8264530375738a57a494399b
RayanIslam30 <rayislam2007@gmail.com>
2026-07-17 09:08:15 -0400

    used different method to calculate fuel

--- fuel.py
+++ fuel.py
@@ -1,58 +1,42 @@
 #Code that takes a fraction and converts to a percentage, similar to how a fuel gauge works. Also returns F on high percentage and E on low percentage
 
 def main():
-    fraction = input("Fraction: ")
-    percentage = convert(fraction)
-    gauge(percentage)
+    #Keep asking for fraction until user gives valid one
+    while True:
+        try:
+           fraction = input("Fraction: ")
+           percentage = convert(fraction)
+           break
+        except (ValueError, ZeroDivisionError):
+           pass
+    #Print out percentage
+    fuel = gauge(percentage) 
+    print(fuel)
 
-#Converts the fraction to a percentage
+#Function to convert fraction to percentage, catching user error in input
 def convert(fraction):
-    try:
-        x, y = fraction.split("/")
-        x = int(x)
-        y = int(y)
-        if y == 0:
-            raise ZeroDivisionError
-        if (x/y) > 1 or (x/y) < 0: #If the fraction is greater than 1 or negative, raise a ValueError
-            raise ValueError
-        return (x / y) * 100
-    except (ValueError, ZeroDivisionError): #If an exception is raised, the program will ask for a fraction again
-        return convert(input("Fraction: "))
+    x, y = fraction.split("/") 
+    x = int(x)
+    y = int(y)
+
+    if y == 0:
+        raise ZeroDivisionError
+    if x < 0 or x > y:
+        raise ValueError
+
+    return x / y * 100
+
 
 #Returns the appropriate gauge reading
 def gauge(percentage):
     if percentage >= 99:
-        print("F")
+        return "F" 
     elif percentage <= 1:
-        print("E")
+        return "E"
     else:
-        print(f"{percentage:.0f}%")
+        return f"{percentage:.0f}%"
 
-main()
 
-#Alternate solution having main do the conversion
+if __name__ == "__main__": #Only run main if file ran directly
+    main()
 
-#def main():
-  #  while True:
-#        try:
-#           fraction = input("Fraction: ")
-#           percentage = convert(fraction)
-#           break
-#        except (ValueError, ZeroDivisionError):
-#           pass
-
-#    gauge(percentage)
-
-#def convert(fraction):
-#    x, y = fraction.split("/")
- #   x = int(x)
-  #  y = int(y)
-#
- #   if y == 0:
-  #      raise ZeroDivisionError
-   # if x < 0 or x > y:
-    #    raise ValueError
-
-    #return x / y * 100
-
-#main()
