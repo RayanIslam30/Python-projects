d7554666c4d656ec76976fa13e96a586b99baafc
RayanIslam30 <rayislam2007@gmail.com>
2026-07-17 09:37:34 -0400

    testing two functions from the same program

---
+++ test_fuel.py
@@ -0,0 +1,33 @@
+#Code to test our two functions in fuel using pytest
+
+import pytest #Import pytest so that we can check exceptions
+
+#Import convert and gauge function from fuel
+from fuel import convert, gauge
+
+#Our tests for the convert function
+def test_convert():
+    assert convert("1/2") == 50 #Check that a normal fraction converts to percentage without %
+    assert convert("1/1") == 100 #Check that it allows for 100
+    assert convert("0/1") == 0 #Check that it allows for 0
+    with pytest.raises(ValueError): #Check that it doesn't take non-integers
+        convert("cat/dog")
+
+    with pytest.raises(ValueError): #Check that x cannot be negative, or exception is raised
+        convert("-1/2")
+
+    with pytest.raises(ValueError): #Check that x/y cannot be greater than 1, or exception is raised
+        convert("2/1")
+
+    with pytest.raises(ZeroDivisionError): #Check that y cannot be 0, or exception is raised
+        convert("1/0")
+
+#Our tests for the gauge function
+def test_gauge():
+    assert gauge(50) == "50%" #Check that a normal case works
+    assert gauge(66.67) == "67%" #Check rounding
+    assert gauge(1) == "E" #Check that <=1 == E
+    assert gauge(99) == "F" #Check that >=99 == F
+
+
+
