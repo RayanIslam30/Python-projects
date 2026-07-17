f288cbb4ad4b37c975b36acb50452ed4e453db83
RayanIslam30 <rayislam2007@gmail.com>
2026-07-16 23:20:35 -0400

    improved bank.py without changing functionality

--- bank.py
+++ bank.py
@@ -1,12 +1,18 @@
 #Code to output $0 when user inputs a string starting with "hello", $20 when user inputs anything else starting with "h", and $100 otherwise
 
-bank = input("Enter greeting: ") #prompt user for input
+def main():
+    bank = input("Enter greeting: ").lower().strip() #prompt user for input, put in lowercase and remove whitespace
+    amount = value(bank)
+    print(f"${amount}")
 
-clean_bank = bank.lower().strip() #put the string in lowercase and remove any whitespace
-#ifelse statement to check if the input is "hello", anything else starting with "h", and print "$0", "$20", or "$100" accordingly
-if clean_bank.startswith("hello"):
-    print("$0")
-elif clean_bank.startswith("h"):
-    print("$20")
-else:
-    print("$100")
+#ifelse statement to check if the input starts with "hello", anything else starting with "h", and print "$0", "$20", or "$100" accordingly
+def value(greeting):
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
+    else:
+        return 100
+
+if __name__ == "__main__": #Only run main if file ran directly
+    main()
--- test_vowel_remover.py
+++ test_vowel_remover.py
@@ -6,3 +6,5 @@ def test_shorten():
         assert shorten("cat") == "ct" #Test lowercase 'a'
         assert shorten("CAT") == "CT" #Test capital 'A'
         assert shorten("aeiouAEIOU") == "" #Test all vowels removed
+        assert shorten("Testing.") == "Tstng." #Test punctuation
+        assert shorten("abc123") == "bc123" #Test numbers
