e54cbb8b61ef445bf2a6e1be1f63600f29b3f24b
RayanIslam30 <rayislam2007@gmail.com>
2026-07-16 22:53:39 -0400

    working with pytest

--- test_vowel_remover.py
+++ test_vowel_remover.py
@@ -1,1 +1,14 @@
 #Code to test our vowel_remover program
+from vowel_remover import shorten #Test the shorten function in vowel_remover
+
+def main(): 
+    test_shorten()
+#Our test of the shorten function
+def test_shorten():
+    if shorten("cat") != "ct":
+        print("cat was not shortened to ct")
+    if shorten("CAT") != "CT":
+        print("CAT was not shortened to CT")
+
+if __name__ == "__main__": #Only run main if file ran directly
+    main()
