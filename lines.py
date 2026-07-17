fddcf7a99fb38a7ba5bca4247d94da97409defa6
RayanIslam30 <rayislam2007@gmail.com>
2026-07-17 17:18:18 -0400

    added basic functionality for line counting

--- lines.py
+++ lines.py
@@ -1,3 +1,12 @@
 #Code to check the amount of lines another program has, not including comments or whitespace
 
+import sys #Import sys to use sys.argv
 lines = int(0) #Initialize our count of lines in the program
+
+name = sys.argv[1] #set our file name to file user inputted
+
+with open(name) as file:
+    for line in file:
+        lines+=1
+
+print(lines)
