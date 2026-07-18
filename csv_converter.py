2a2ab36b37a2f071695932f2a5b4a7f19c706712
RayanIslam30 <rayislam2007@gmail.com>
2026-07-18 12:59:42 -0400

    added sorting ability to csv_converter

--- csv_converter.py
+++ csv_converter.py
@@ -6,7 +6,7 @@ import csv #Import csv module to work with our csv files
 #Check that sys.argv[1] and sys.argv[2] exists
 if len(sys.argv) == 3: #If file is valid, set name variable to file name
     name = sys.argv[1] #set our file name to file user inputted
-    output = sys.argv[2] 
+    output = sys.argv[2] #set our new file to write
 elif len(sys.argv) < 3: #If sys.argv[1] or [2] doesn't exist, quit with too few arguments
     sys.exit("Too few command-line arguments")
 else: #If there are more than 2 arguments, quit with too many arguments
@@ -16,4 +16,14 @@ else: #If there are more than 2 arguments, quit with too many arguments
 if not name.endswith(".csv"):
     sys.exit("Not a CSV file")
 
-    
+#If those checks pass, then try and read the csv file
+try:
+    with open(name) as file:
+        reader = csv.DictReader(file) #use dictionary reader to read the key on the top row of csv file 
+        for row in reader:
+            last, first = row["name"].split(",")
+            first = first.strip()
+            print(first, last, row["house"])
+except FileNotFoundError:
+    sys.exit("File does not exist")
+
