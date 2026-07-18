274b70e6889ab4d886e3e0dfa2436c0b9314db6c
RayanIslam30 <rayislam2007@gmail.com>
2026-07-18 19:38:06 -0400

    added more checks for valid input for image modification

--- image_modifier.py
+++ image_modifier.py
@@ -7,9 +7,23 @@ from PIL import Image, ImageOps #Import image opening and resizing/cropping
 
 #Check that sys.argv[1] and sys.argv[2] exists
 if len(sys.argv) == 3: #If file is valid, set name variable to file name
-    name = sys.argv[1] #set our file name to file user inputted
-    output = sys.argv[2] #set our new file to write
+    input = sys.argv[1] #set image user wants modified
+    output = sys.argv[2] #set location for new image
 elif len(sys.argv) < 3: #If sys.argv[1] or [2] doesn't exist, quit with too few arguments
     sys.exit("Too few command-line arguments")
 else: #If there are more than 2 arguments, quit with too many arguments
     sys.exit("Too many command-line arguments")
+    
+#Get our file endings to use for checks
+input_ext = os.path.splitext(sys.argv[1])[1].lower() #Get file ending of original image
+output_ext= os.path.splitext(sys.argv[2])[1].lower() #Get file ending of where the new image will be
+
+if input_ext not in [".jpg", ".jpeg", ".png"] or output_ext not in [".jpg", ".jpeg", ".png"]: #Check that file endings are either jpg or png
+    sys.exit("Invalid input")
+
+if input_ext != output_ext: #If input ending is not equal to output ending, quit
+    sys.exit("Input and output have different extensions")
+
+#If all those checks passed, start the overlay process
+shirt = Image.open("images/shirt.png") #Get the shirt image from images folder
+
