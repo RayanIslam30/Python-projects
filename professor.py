6f14c7d9a7a900d10797ea2630503d432e92da79
RayanIslam30 <rayislam2007@gmail.com>
2026-07-19 14:41:28 -0400

    reformatted with black

--- professor.py
+++ professor.py
@@ -1,17 +1,18 @@
-#Code that allows the user to choose between 3 levels, and solve 10 random math problems for that level. Mimicks the educational toy 'little professor'
+# Code that allows the user to choose between 3 levels, and solve 10 random math problems for that level. Mimicks the educational toy 'little professor'
 
-import random #Import random module to get random numbers 
+import random  # Import random module to get random numbers
+
 
 def main():
-    level = get_level() 
-    score = int(0) #Initialize our points counter
-#Ask our 10 questions 
+    level = get_level()
+    score = int(0)  # Initialize our points counter
+    # Ask our 10 questions
     for _ in range(10):
-        #Get random integers
+        # Get random integers
         x = generate_integer(level)
         y = generate_integer(level)
 
-    #Give user the question, they have three chances to get it right
+        # Give user the question, they have three chances to get it right
         for _ in range(3):
             try:
                 guess = int(input(f"{x} + {y} = "))
@@ -23,33 +24,35 @@ def main():
                 break
             else:
                 print("EEE")
-        else: #If they get it wrong three times, tell them the answer
+        else:  # If they get it wrong three times, tell them the answer
             print(f"{x} + {y} = {x + y}")
     print(f"Score: {score}")
 
-#Function to get our level
+
+# Function to get our level
 def get_level():
     while True:
         try:
             level = int(input("Level: "))
             # User chooses a level, either 1, 2, or 3
-            if level in [1,2,3]:
+            if level in [1, 2, 3]:
                 return level
         except ValueError:
             pass
 
 
-#Function to generate random numbers based on level
+# Function to generate random numbers based on level
 def generate_integer(level):
-    if level == 1: #Level 1, single-digit numbers
-        int = random.randint(0,9)
+    if level == 1:  # Level 1, single-digit numbers
+        int = random.randint(0, 9)
         return int
-    elif level == 2: #Level 2, double-digit numbers
-        int = random.randint(10,99)
+    elif level == 2:  # Level 2, double-digit numbers
+        int = random.randint(10, 99)
         return int
-    else: #otherwise must be level 3, triple-digit numbers
-        int = random.randint(100,999)
+    else:  # otherwise must be level 3, triple-digit numbers
+        int = random.randint(100, 999)
         return int
 
+
 if __name__ == "__main__":
-    main() 
+    main()
