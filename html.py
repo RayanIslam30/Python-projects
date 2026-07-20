ec15e1841f851888cbe281f09db185f7b434b475
RayanIslam30 <rayislam2007@gmail.com>
2026-07-20 15:35:03 -0400

    program to convert html to youtube link

---
+++ html.py
@@ -0,0 +1,17 @@
+#Code that takes HTML output from youtube, and gives back just a sharable url to go to that video on youtube
+import re 
+import sys
+
+
+def main():
+    print(parse(input("HTML: "))) #Ask for HTML output, and call our function to read it
+
+
+def parse(html):
+    #Check if we have long or short output
+    short = 
+
+
+if __name__ == "__main__": #Only run if called directly
+    main()
+
--- test_ip_address.py
+++ test_ip_address.py
@@ -10,4 +10,6 @@ def test_value():
     assert validate("0.0.0.0.") == "Invalid" #An extra '.' will make invalid
     assert validate("-100.-2.-9.-234") == "Invalid" #Numbers less than 0 will be invalid
     assert validate("300.407.519.789") == "Invalid" #Numbers greater than 255 will be invalid
+    assert validate("200.300.400.500") == "Invalid" #All numbers must be valid, not just first
     assert validate("cat.dog.sheep.fish") == "Invalid" #Must have numbers
+    
