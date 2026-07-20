928f831cfa0230edc237b8a4fe83a18dc4458b4f
RayanIslam30 <rayislam2007@gmail.com>
2026-07-20 18:23:26 -0400

    fixed error in time conversion for 12 AM and PM

--- test_time_converter.py
+++ test_time_converter.py
@@ -9,6 +9,7 @@ def test_convert_hour(): #Run tests without adding minutes to our times
     assert convert("7 AM to 6 PM") == "07:00 to 18:00"
     assert convert("2 AM to 9 AM") == "02:00 to 09:00"
     assert convert("3 PM to 11 PM") == "15:00 to 23:00"
+    assert convert("12 AM to 12 PM") == "00:00 to 12:00"
 
 def test_convert_minute(): #Run tests with hours and minutes to the times, as well as mixed inputs
     assert convert("8:30 AM to 7:30 PM") == "08:30 to 19:30"
--- time_converter.py
+++ time_converter.py
@@ -25,14 +25,18 @@ def convert(time):
                     raise ValueError
                 if minute not in range(0,60): #If minute isn't valid minute, raise value error
                     raise ValueError
-                if start_ampm == "PM" or start_ampm == "pm":
+                if (start_ampm == "PM" or start_ampm == "pm") and hour != 12: #Convert from AM/PM to 24 hour
                     hour+=12
+                if hour == 12 and (start_ampm == "AM" or start_ampm == "am"): #Catch 12 AM, should go to 00:00 in 24 hour
+                    hour-=12
             else: #We just got hour for first time
                 hour = int(start_time)
                 if hour not in range(1,13): #If hour isn't valid hour, raise value error
                     raise ValueError 
-                if start_ampm == "PM" or start_ampm == "pm":
+                if (start_ampm == "PM" or start_ampm == "pm") and hour != 12: #Convert from AM/PM to 24 hour
                     hour+=12
+                if hour == 12 and (start_ampm == "AM" or start_ampm == "am"): #Catch 12 AM, should go to 00:00 in 24 hour
+                    hour-=12
 
             if ":" in end_time: #If we got hours and minutes for second time
                 hour2, minute2 = end_time.split(":")
@@ -42,14 +46,18 @@ def convert(time):
                  raise ValueError
                 if minute2 not in range(0,60): #If minute isn't valid minute, raise value error
                   raise ValueError
-                if end_ampm == "PM" or end_ampm == "pm":
+                if (end_ampm == "PM" or end_ampm == "pm") and hour2 != 12: #Convert from AM/PM to 24 hour
                     hour2+=12
+                if hour == 12 and (start_ampm == "AM" or start_ampm == "am"): #Catch 12 AM, should go to 00:00 in 24 hour
+                    hour-=12
             else: #We just got hour for first time
                 hour2 = int(end_time)
                 if hour2 not in range(1,13): #If hour isn't valid hour, raise value error
                     raise ValueError
-                if end_ampm == "PM" or end_ampm == "pm":
+                if (end_ampm == "PM" or end_ampm == "pm") and hour2 != 12: #Convert from AM/PM to 24 hour
                  hour2+=12
+                if hour2 == 12 and (end_ampm == "AM" or end_ampm == "am"): #Catch 12 AM, should go to 00:00 in 24 hour
+                    hour2-=12
         else: #If input isn't valid, raise value error
             raise ValueError
         
