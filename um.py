844db9bf2d603c235b04c3de185c9c767a03fccc
RayanIslam30 <rayislam2007@gmail.com>
2026-07-20 18:46:18 -0400

    added program to count the amount of 'um' in a string

---
+++ um.py
@@ -0,0 +1,17 @@
+#Code to count the amount of times 'um' shows up in a string
+
+import re
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(str):
+    matches = re.findall(r"\bum\b", str, re.IGNORECASE) #Finds only the word 'um' in the string, not as part of another word
+    return len(matches)
+
+
+
+if __name__ == "__main__":
+    main()
