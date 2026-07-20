31ef6d96c47f60c8bb6db1969ef4ed9ffa1bb5ef
RayanIslam30 <rayislam2007@gmail.com>
2026-07-20 15:52:18 -0400

    parses html output, and isolates the embed to create a youtube link

--- html.py
+++ html.py
@@ -1,17 +1,23 @@
-#Code that takes HTML output from youtube, and gives back just a sharable url to go to that video on youtube
-import re 
+# Code that takes HTML output from youtube, and gives back just a sharable url to go to that video on youtube
+import re
 import sys
 
 
 def main():
-    print(parse(input("HTML: "))) #Ask for HTML output, and call our function to read it
+    print(
+        parse(input("HTML: "))
+    )  # Ask for HTML output, and call our function to read it
 
 
 def parse(html):
-    #Check if we have long or short output
-    if short := re.search(r'<iframe.*?src="https?://(?:www\.)?youtube\.com/embed/([^"]+)"', html): #Check if our string meets all requirements for short html
+    # Same check for short and long input
+    if short := re.search(
+        r'<iframe.*?src="https?://(?:www\.)?youtube\.com/embed/([^"]+)"', html
+    ):  # Check if html formatted right, get the link embed and ignore everything else
         return f"https://youtu.be/{short.group(1)}"
+    else:  # Otherwise, not valid
+        return "None"
 
-if __name__ == "__main__": #Only run if called directly
+
+if __name__ == "__main__":  # Only run if called directly
     main()
-
