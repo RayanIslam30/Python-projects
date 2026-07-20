312d5719106c11fb264bf942b1ddc590f5b4539a
RayanIslam30 <rayislam2007@gmail.com>
2026-07-20 18:07:01 -0400

    reformatted time converter and test

--- test_time_converter.py
+++ test_time_converter.py
@@ -10,7 +10,13 @@ def test_convert_hour(): #Run tests without adding minutes to our times
     assert convert("2 AM to 9 AM") == "02:00 to 09:00"
     assert convert("3 PM to 11 PM") == "15:00 to 23:00"
 
-
 def test_convert_minute(): #Run tests with hours and minutes to the times, as well as mixed inputs
-    assert convert("8:00 AM to 7:00 PM") == "08:00 to 19:00"
-def test_value_error(): #Give incorrect
+    assert convert("8:30 AM to 7:30 PM") == "08:30 to 19:30"
+    assert convert("5:47 AM to 9:28 PM") == "05:47 to 21:28"
+    assert convert("5:00 AM to 10:00 AM") == "05:00 to 10:00"
+
+def test_value_error(): #Give incorrect input to see if it is caught
+    assert convert("2-10 PM") == "Invalid input"
+    assert convert("cat AM to dog PM") == "Invalid input"
+    assert convert("13 AM to 19 PM") == "Invalid input"
+    assert convert("8:90 AM to 5:64 PM") == "Invalid input"
--- time_converter.py
+++ time_converter.py
@@ -10,47 +10,51 @@ def convert(time):
     #Set up baseline values for output at the end, in case they don't get overwritten
     minute = int(0)
     minute2 = int(0)
-    if time := re.search(r"^(\d{1,2}|\d{1,2}:\d{2}) (AM|PM) to (\d{1,2}|\d{1,2}:\d{2}) (AM|PM)$", time, re.IGNORECASE): #Check that user inputted what should be a valid time
+    try:
+        if time := re.search(r"^(\d{1,2}|\d{1,2}:\d{2}) (AM|PM) to (\d{1,2}|\d{1,2}:\d{2}) (AM|PM)$", time, re.IGNORECASE): #Check that user inputted what should be a valid time
         #Define variables
-        start_time = time.group(1)
-        start_ampm = time.group(2)
-        end_time = time.group(3)
-        end_ampm = time.group(4)
-        if ":" in start_time: #If we got hours and minutes for first time
-            hour, minute = start_time.split(":")
-            hour = int(hour)
-            minute = int(minute)
-            if hour not in range(1,13): #If hour isn't valid hour, raise value error
-                raise ValueError("Invalid hour")
-            if minute not in range(0,60): #If minute isn't valid minute, raise value error
-                raise ValueError("Invalid minute")
-            if start_ampm == "PM" or start_ampm == "pm":
-                hour+=12
-        else: #We just got hour for first time
-            hour = int(start_time)
-            if hour not in range(1,13): #If hour isn't valid hour, raise value error
-                raise ValueError("Invalid hour") 
-            if start_ampm == "PM" or start_ampm == "pm":
-                hour+=12
+            start_time = time.group(1)
+            start_ampm = time.group(2)
+            end_time = time.group(3)
+            end_ampm = time.group(4)
+            if ":" in start_time: #If we got hours and minutes for first time
+                hour, minute = start_time.split(":")
+                hour = int(hour)
+                minute = int(minute)
+                if hour not in range(1,13): #If hour isn't valid hour, raise value error
+                    raise ValueError
+                if minute not in range(0,60): #If minute isn't valid minute, raise value error
+                    raise ValueError
+                if start_ampm == "PM" or start_ampm == "pm":
+                    hour+=12
+            else: #We just got hour for first time
+                hour = int(start_time)
+                if hour not in range(1,13): #If hour isn't valid hour, raise value error
+                    raise ValueError 
+                if start_ampm == "PM" or start_ampm == "pm":
+                    hour+=12
 
-        if ":" in end_time: #If we got hours and minutes for second time
-            hour2, minute2 = end_time.split(":")
-            hour2 = int(hour2)
-            minute2 = int(minute2)
-            if hour2 not in range(1,13): #If hour isn't valid hour, raise value error
-                raise ValueError("Invalid hour")
-            if minute2 not in range(0,60): #If minute isn't valid minute, raise value error
-                raise ValueError("Invalid minute")
-            if end_ampm == "PM" or end_ampm == "pm":
-                hour2+=12
-        else: #We just got hour for first time
-            hour2 = int(end_time)
-            if hour2 not in range(1,13): #If hour isn't valid hour, raise value error
-                raise ValueError("Invalid hour") 
-            if end_ampm == "PM" or end_ampm == "pm":
-                hour2+=12
-    else: #If input isn't valid, raise value error
-        raise ValueError("Invalid input")
+            if ":" in end_time: #If we got hours and minutes for second time
+                hour2, minute2 = end_time.split(":")
+                hour2 = int(hour2)
+                minute2 = int(minute2)
+                if hour2 not in range(1,13): #If hour isn't valid hour, raise value error
+                 raise ValueError
+                if minute2 not in range(0,60): #If minute isn't valid minute, raise value error
+                  raise ValueError
+                if end_ampm == "PM" or end_ampm == "pm":
+                    hour2+=12
+            else: #We just got hour for first time
+                hour2 = int(end_time)
+                if hour2 not in range(1,13): #If hour isn't valid hour, raise value error
+                    raise ValueError
+                if end_ampm == "PM" or end_ampm == "pm":
+                 hour2+=12
+        else: #If input isn't valid, raise value error
+            raise ValueError
+        
+    except(ValueError):
+        return("Invalid input")
         
     #Print out our converted time
     return(f"{hour:02}:{minute:02} to {hour2:02}:{minute2:02}")
