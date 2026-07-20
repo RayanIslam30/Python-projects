870e9e6e3e7fd4c9e1736faa6c85fc4b37dc4203
RayanIslam30 <rayislam2007@gmail.com>
2026-07-19 22:24:53 -0400

    using re to check strings with criteria

---
+++ Ip_address.py
@@ -0,0 +1,25 @@
+#Code that checks if a IP address is valid or invalid
+
+import re #Import re to check for validity
+
+
+def main():
+    ip = input("IPv4 Address: ").strip()
+    print(validate(ip))
+    
+
+def validate(ip):
+    if re.search(r"^\d+\.\d+\.\d+\.\d+$", ip): #Checks that ip address is formatted right, and numbers are where we expect
+        parts = ip.split(".") #Split our string into number parts
+        for part in parts:
+            number = int(part)
+            if not (0 <= number <= 255): #Check that each number is valid
+                return("Invalid") #Not valid numbers, invalid
+        return("Valid") #If ip address is valid format and numbers, valid
+    else: #If ip address formatted wrong, invalid
+        return("Invalid")
+   
+
+
+if __name__ == "__main__": #Only run file if called directly, so that we can run tests
+    main()
---
+++ test_ip_address.py
@@ -0,0 +1,13 @@
+#Code to test our IP address checker using pytest
+
+from Ip_address import validate #Import the validate function from Ip_address
+
+#Our tests for the validate function
+
+def test_value():
+    assert validate("255.255.255.255") == "Valid" #Valid IP address is valid
+    assert validate("0.153.194.255") == "Valid" #Another Valid IP address is valid
+    assert validate("0.0.0.0.") == "Invalid" #An extra '.' will make invalid
+    assert validate("-100.-2.-9.-234") == "Invalid" #Numbers less than 0 will be invalid
+    assert validate("300.407.519.789") == "Invalid" #Numbers greater than 255 will be invalid
+    assert validate("cat.dog.sheep.fish") == "Invalid" #Must have numbers
