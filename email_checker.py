812af0f1d244cc61087f24700732b7508883080c
RayanIslam30 <rayislam2007@gmail.com>
2026-07-20 19:06:38 -0400

    program that uses a validation module to determine if an email is valid

---
+++ email_checker.py
@@ -0,0 +1,17 @@
+#Code that uses a premade validator to check if an email is valid
+
+from validator_collection import is_email #From the validator_collection module, import a checker for valid email
+
+def main(): 
+    print(email(input("What's your email address? ")))
+
+def email(email):
+    if valid_email := (is_email(email)):
+        return "Valid"
+    else:
+        return "Invalid"
+
+
+    
+if __name__ == "__main__":
+    main()
