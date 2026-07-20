d364a9fa9532aec620d2897e23e508e3a2781b58
RayanIslam30 <rayislam2007@gmail.com>
2026-07-20 18:53:52 -0400

    added tests for um_count

---
+++ test_um_count.py
@@ -0,0 +1,18 @@
+#Code to test our um program
+
+from um_count import count #Import the count function from um
+
+#Our tests for the countfunction
+
+
+def test_um_count(): #Run normal test
+    assert count("um") == 1 
+    assert count("um um um") == 3
+
+def test_um_words(): #Test including other words and punctuation
+    assert count("hello, um, world") == 1 
+    assert count("um, I, um... um!") == 3
+
+def test_um_in_word(): #Test that um is not counted as part of a word
+    assert count("strawberry cake is, um, yummy") == 1 
+    assert count("yum") == 0
--- um_count.py
+++ um_count.py
@@ -2,16 +2,12 @@
 
 import re
 
-
-def main():
+def main(): 
     print(count(input("Text: ")))
-
 
 def count(str):
     matches = re.findall(r"\bum\b", str, re.IGNORECASE) #Finds only the word 'um' in the string, not as part of another word
     return len(matches)
-
-
 
 if __name__ == "__main__":
     main()
