f7fab0ccf558c6c42627a2a1fbdf21d0dbf7e5b8
RayanIslam30 <rayislam2007@gmail.com>
2026-07-23 10:06:07 -0400

    added feature to compare dates and find minutes between them

--- minutes_since.py
+++ minutes_since.py
@@ -13,13 +13,17 @@ def calculate(date):
         month = int(month)
         day = int(day)
         try:
-            datetime.date(year,month,day) #Check if we have a valid date
+            birthdate = datetime.date(year,month,day) #Check if we have a valid date
+            today = datetime.date.today() #Get todays date
         except(ValueError):
             return("Invalid date")
     else: #If not, invalid
         return("Invalid date")
+    time = today - birthdate #Get the difference between the dates
+    minutes = time.days * 24 * 60 #Convert difference to minutes
+    p = inflect.engine() #Run inflect to convert number to words
+    minutes_since = p.number_to_words(minutes, andword="") 
+    return minutes_since.capitalize() + " minutes" #Return the output in proper format
     
-
-
 if __name__ == "__main__": #Only run main if file ran directly
     main()
