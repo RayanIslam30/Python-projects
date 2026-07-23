9cd8d6c60ae099b8dde1c05b730b332aa2cd3504
RayanIslam30 <rayislam2007@gmail.com>
2026-07-23 10:13:11 -0400

    improved design

--- minutes_since.py
+++ minutes_since.py
@@ -2,13 +2,14 @@
 
 import datetime #Import the datetime module
 import inflect #Use to convert number into words
+import sys 
 
 def main():
     print(calculate(input("Date: ")))
 
-def calculate(date):
-    if date.count("-") == 2: #Check if format is at least close to what we want
-        year, month, day = date.split("-") #Split into year, month, day by splitting at '-'
+def calculate(date_str):
+    if date_str.count("-") == 2: #Check if format is at least close to what we want
+        year, month, day = date_str.split("-") #Split into year, month, day by splitting at '-'
         year = int(year)
         month = int(month)
         day = int(day)
@@ -16,9 +17,9 @@ def calculate(date):
             birthdate = datetime.date(year,month,day) #Check if we have a valid date
             today = datetime.date.today() #Get todays date
         except(ValueError):
-            return("Invalid date")
+            sys.exit("Invalid date")
     else: #If not, invalid
-        return("Invalid date")
+        sys.exit("Invalid date")
     time = today - birthdate #Get the difference between the dates
     minutes = time.days * 24 * 60 #Convert difference to minutes
     p = inflect.engine() #Run inflect to convert number to words
