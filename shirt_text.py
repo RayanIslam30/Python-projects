f6966cad90c2b1e12093c64df979d210c3450ca8
RayanIslam30 <rayislam2007@gmail.com>
2026-07-24 16:45:20 -0400

    printing text onto a pdf

--- shirt_text.py
+++ shirt_text.py
@@ -6,11 +6,12 @@ pdf = FPDF(orientation="P", format="A4") #Get a blank A4 portrait page
 pdf.add_page()
 
 pdf.set_font("Helvetica", size=30) #Set the font to Helvetica 
-pdf.cell(text="Test string")
+pdf.ln(5) #Move down 5 mm
+pdf.cell(w=0,text="Test string", align="C") #Print this string at the top center of the page
 
 
 
 pdf.set_font("Helvetica", "B", size=20) #Set font to Helvetica bold, smaller
 pdf.set_text_color(255,255,255) #Set text color to white
 
-pdf.output("test.pdf")
+pdf.output("images/test.pdf")
