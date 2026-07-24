5b89da8c883f3184aa745a9a19be569172dea44b
RayanIslam30 <rayislam2007@gmail.com>
2026-07-24 16:40:24 -0400

    changed file name to allow for module to access actual html

--- shirt_text.py
+++ shirt_text.py
@@ -5,5 +5,12 @@ from fpdf import FPDF #Module to write text onto image
 pdf = FPDF(orientation="P", format="A4") #Get a blank A4 portrait page
 pdf.add_page()
 
-pdf.set_font("Helvetica", "B", 20) #Set the font to Helvetica Bold
+pdf.set_font("Helvetica", size=30) #Set the font to Helvetica 
+pdf.cell(text="Test string")
+
+
+
+pdf.set_font("Helvetica", "B", size=20) #Set font to Helvetica bold, smaller
 pdf.set_text_color(255,255,255) #Set text color to white
+
+pdf.output("test.pdf")
