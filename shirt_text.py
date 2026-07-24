5b87392533cb0b9d8f20acf7b66745db146206c5
RayanIslam30 <rayislam2007@gmail.com>
2026-07-24 16:33:07 -0400

    working with a module to put text on pdf

--- shirt_text.py
+++ shirt_text.py
@@ -1,1 +1,9 @@
 #Code that puts text onto a blank shirt, signifying that a user inputted string 'took CS50'. The shirt is a sort of 'certificate'
+
+from fpdf import FPDF #Module to write text onto image
+
+pdf = FPDF(orientation="P", format="A4") #Get a blank A4 portrait page
+pdf.add_page()
+
+pdf.set_font("Helvetica", "B", 20) #Set the font to Helvetica Bold
+pdf.set_text_color(255,255,255) #Set text color to white
