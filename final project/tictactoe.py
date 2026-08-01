0e7f257eeff9c39332f13930a9feb9a6636552d6
RayanIslam30 <rayislam2007@gmail.com>
2026-07-31 21:14:03 -0400

    working on logic for player-vs-player

--- final project/tictactoe.py
+++ final project/tictactoe.py
@@ -42,17 +42,20 @@ class Board:
         (0, 4, 8),
         (2, 4, 6)
     ]
+        for a, b, c in winning_conditions:
+            if self.board[a] == self.board[b] == self.board[c] and self.board[a] in ["X", "O"]: #If any winning combination is either all X or O, then set variable to true
+                return True
+        return False #If we didn't find a winner, return false
         
 
-    def is_full(self): #Check if the board is full
-        ...
+    def is_full(self): #Check if the board is full to determine a tie
+        return all(square in ["X", "O"] for square in self.board) #If all squares are either X or O, then the board is full
 
 board = Board() 
 def main():
     player2 = False #Initialize variable to check if we have two player mode enabled or not
-    print("How to play: ") #Instructions on how it works
     while True: #Loop until we get a valid difficulty or two player mode is selected
-        mode = input("Choose difficulty (easy, normal, hard): ").lower().strip() #User chooses initial difficulty
+        mode = input("Choose mode (easy, normal, hard, or two-player): ").lower().strip() #User chooses initial difficulty
         if mode == "2p" or mode == "two-player" or mode =="two player": #Check for two player mode
             player2 = True
             break
@@ -66,10 +69,13 @@ def get_user_move(board):
         try:
             position = int(input("Player 1 (X), enter your move: "))
             position-=1 #Our value is actually 0 indexed, so lower number by 1
+
             if position in board.available_moves(): #Check that the position is unoccupied
                 break
-            else:
+            elif 0<=position<=8:
                 print("That square is already occupied.")
+            else:
+                print("Please choose valid square.")
         except(ValueError):
             print("Please choose valid square.")
     board.make_move((position), "X") 
@@ -82,56 +88,67 @@ def get_computer_move(board, mode):
     ...
 
 #The main gameplay loop. Different logic for player vs computer and player vs player, and checks for win or tie after every move. Gets difficulty 
-def play_game(player2, mode):
+def play_game(player2, mode): 
     if player2: #If two player mode
-        while True:
+        while True: #Loop until we get a winner or tie
             print("\n") #New line for spacing
             board.display() 
             print("\n") #new line for spacing
 
         # Player 1's turn
             get_user_move(board)
-            board.display() #Display board with player's move
+            print("\n") #new line for spacing
+            board.display() #Display board with player's move 
         # Check if player 1 won
-            ...
-
+            winner = board.check_winner() #Check for a winner   
+            if winner:
+                return("Player 1 wins!")
         # Check for tie
-            ...
+            if board.is_full():
+                return("It's a tie!")
 
         # Player 2's turn
             get_user2_move(board)
-
+            board.display() #Display board with player's move
     # Check if player 2 won
-            ...
+            winner = board.check_winner() #Check for a winner   
+            if winner:
+                return("Player 2 wins!")
 
     # Check for tie
-            ...
+            if board.is_full():
+                return("It's a tie!")
 
-            break #temporary break to prevent infinite loop
     else: #If player vs computer
-        while True:
+        while True: #Loop until we get a winner or tie
             print("\n") #new line for spacing
             board.display()
             print("\n") #new line for spacing
 
         # Player's turn
             get_user_move(board)
-
+            board.display() #Display board with player's move
         # Check if player won
-            ...
+            winner = board.check_winner()
+            if winner:
+                return("Player wins!")
 
         # Check for tie
-            ...
+            if board.is_full():
+                return("It's a tie!")
 
         # Computer's turn
             get_computer_move(board, mode)
+            board.display() #Display board with computer's move
 
         # Check if computer won
-            ...
+            winner = board.check_winner()
+            if winner:
+                return("Computer wins!")
 
         # Check for tie
-            ...
+            if board.is_full():
+                return("It's a tie!")
         
-            break #temporary break to prevent infinite loop
 if __name__ == "__main__": #Only run main if called directly, allows us to test functions
     main()
